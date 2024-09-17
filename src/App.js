c34df099a1b92c325543cf83c1fa44ccf10abfd3
Bruce <irabruce10@gmail.com>
2024-09-17 08:10:23 +0200

    Adding styled components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
+//  Importng necessary modules and state
+
 import { useState } from 'react';
 import './App.css';
 
 import randomColor from 'randomcolor';
 
 export default function App() {
+  // Using React Hooks to manage the state of the application
   const [color, setColor] = useState('#000000');
   const [luminosity, setLuminosity] = useState('');
   const [hue, setHue] = useState('red');
+
+  // Setting up parameters for randomColor function
 
   const param = {
     luminosity: luminosity,
     hue: hue,
   };
-
+  // Function to generate random color with user input hue and luminosity
   function colorHandle() {
     const colorGenerate = randomColor(param);
     setColor(colorGenerate);
